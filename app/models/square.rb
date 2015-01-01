474152411cf5e06080afd0fe02e62754ea63eec9
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-01-01 10:57:26 +0000

    Square#initialize assigns coords.

--- app/models/square.rb
+++ app/models/square.rb
@@ -1,3 +1,11 @@
 class Square < ActiveRecord::Base
   belongs_to :board
+
+  def initialize([x_coord, y_coord, z_coord])
+    self.x_coord = x_coord
+    self.y_coord = y_coord
+    self.z_coord = z_coord
+
+    self.mark = nil
+  end
 end
