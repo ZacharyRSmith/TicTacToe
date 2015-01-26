a33859237771887d15cf70dfe51630d27bf06e39
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-01-26 13:09:34 +0000

    Refactor conditional to filter in line#set_status.

--- app/models/line.rb
+++ app/models/line.rb
@@ -9,24 +9,27 @@ class Line < ActiveRecord::Base
   def set_status(new_mark)
     if (new_mark != "X" && new_mark != "O")
       self.status = "non_scoreable"
-    else
-      case self.status
-      when "unmarked"
-        if new_mark == "X"
-          self.status = "x_scoreable"
-        else
-          self.status = "o_scoreable"
-        end
-      when "x_scoreable"
-        if new_mark == "O"
-          self.status = "non_scoreable"
-        end
-      when "o_scoreable"
-        if new_mark == "X"
-          self.status = "non_scoreable"
-        end
+      self.save
+      return
+    end
+
+    case self.status
+    when "unmarked"
+      if new_mark == "X"
+        self.status = "x_scoreable"
+      else
+        self.status = "o_scoreable"
+      end
+    when "x_scoreable"
+      if new_mark == "O"
+        self.status = "non_scoreable"
+      end
+    when "o_scoreable"
+      if new_mark == "X"
+        self.status = "non_scoreable"
       end
     end
+
     self.save
   end
 end
