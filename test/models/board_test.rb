7053fc388b1a7f038e6c7331a95e6a2dfe6bf685
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-01-27 16:00:16 +0000

    Split a BoardTest into two tests.

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -5,24 +5,15 @@ class BoardTest < ActiveSupport::TestCase
     @board = Board.create!({ size: 2 })
   end
 
-  test "board.lines and square.lines should be correct count" do
-    # 4 each of:
-    # columns
-    # diagonal-columns
-    # rows
-    # diagonal-rows
-    # diagonals
-    # diagonal-diagonals
-    # hills
-
-#     board = Board.create!({ size: 2 })
+  test "board.lines should be correct count" do
     assert_equal 28, @board.lines.count
+  end
 
+  test "square.lines should be correct count" do
     sqr = @board.squares.find(@board.squares_id_ary[0][0][0])
     assert_equal 7, sqr.lines.count
   end
 
-  
   test "square should have correct self.ai_priority" do
     board = Board.create!({ size: 3 })
 
