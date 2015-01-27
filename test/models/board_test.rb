7cf1f683a35589083f0d5acf25ad880fea385063
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-01-27 21:00:55 +0000

    Move SquareTests from 'board_test.rb' to 'square_test.rb'.

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -8,18 +8,4 @@ class BoardTest < ActiveSupport::TestCase
   test "board.lines should be correct count" do
     assert_equal 28, @board.lines.count
   end
-
-  test "square.lines should be correct count" do
-    sqr = @board.squares.find(@board.squares_id_ary[0][0][0])
-    assert_equal 7, sqr.lines.count
-  end
-
-  test "square should have correct self.ai_priority" do
-    board = Board.create!({ size: 3 })
-
-    sqr = board.squares.find(board.squares_id_ary[0][0][0])
-    sqr.set_ai_priority()
-    sqr.save
-    assert_equal 12, sqr.ai_priority
-  end
 end
--- test/models/square_test.rb
+++ test/models/square_test.rb
@@ -6,4 +6,22 @@ class SquareTest < ActiveSupport::TestCase
   should validate_presence_of(:y_coord)
   should validate_presence_of(:z_coord)
   should validate_presence_of(:mark)
+
+  def setup
+    @board = Board.create!({ size: 2 })
+  end
+
+  test "square.lines should be correct count" do
+    sqr = @board.squares.find(@board.squares_id_ary[0][0][0])
+    assert_equal 7, sqr.lines.count
+  end
+
+  test "square should have correct self.ai_priority" do
+    board = Board.create!({ size: 3 })
+
+    sqr = board.squares.find(board.squares_id_ary[0][0][0])
+    sqr.set_ai_priority()
+    sqr.save
+    assert_equal 12, sqr.ai_priority
+  end
 end
